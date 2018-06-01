7677b213e677cbdb3eb85e80e567965158400d40
AlexCD7 <adc.calispa@yavirac.edu.ec>
2018-06-01 08:08:26 -0500

    primer commit

---
+++ Futbolista.cs
@@ -0,0 +1,14 @@
+using System;
+class Futbolista
+    {
+            float estatura =179;
+            int edad = 29;
+            string posicion ="Delantero";
+            
+            
+            public void Jugar()
+            {
+               Console.WriteLine("El futbolista está jugando");
+                //TODO = Por hacer
+            }
+    }
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myFutbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clase MyFutbol");
+            Balon adidas = new Balon();
+            adidas.Inflar();
+           
+
+            Futbolista  Messi = new Futbolista();
+            Messi.Jugar();
+
+            Cancha Old = new Cancha();
+            Old.Sostener();
+
+            Camiseta Manchester = new Camiseta();
+            Manchester.Vestir();
+
+            Botines Nike = new Botines();
+            Nike.Proteger();
+        }
+    }
+}
